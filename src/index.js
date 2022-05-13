d4e40a47971b3d5b580f0503513158b5c21cba81
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-05-13 18:18:28 +0100

    Started the markup of the project and checked my api key if its working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+const cood = {}
+
+async function getWeather() {
+  const dataRaw = await fetch ('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=6c4d940e4de82452ff470fc4841c5d68', {mode: 'cors'})
+  const data = await dataRaw.json()
+  cood.lat = data.coord.lat
+  cood.lon = data.coord.lon
+  console.log(data)
+  
+  const longLat = await fetch (`https://api.openweathermap.org/data/2.5/onecall?lat=${cood.lat}&lon=${cood.lon}&exclude=minutely&appid=6c4d940e4de82452ff470fc4841c5d68`, {mode: 'cors'})
+  const longLatnew = await longLat.json()
+  console.log(longLatnew)
+}
+
+
+
+getWeather()
+
+console.log(cood)
+
